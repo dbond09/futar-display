9d78d71cf12980fa78aa89f4eb315b7e0bf55d1b
Botond Diviki-Nagy <boti0909@gmail.com>
2019-09-03 15:17:56 +0200

    added kerning

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
+
 const DOT_RADIUS = 6;
 const ROWS = 20;
 const COLUMNS = 50;
@@ -15,35 +21,52 @@ function draw() {
     last = performance.now();
     ctx.fillStyle = '#1a1a1a';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // if (state == 1) {
-    // //   ctx.fillStyle = 'orange';
-    //   content = '4444';
-    //   state = 0;
-    // }
-    // else {
-    // //   ctx.fillStyle = 'black';
-    //   content = 'aaaa';
-    //   state = 1;
-    // }
-  // ctx.fillRect(100, 100, 200, 200);
-    for (var i = 0; i < content.length; i++) {
-      for (var x = 0; x < CHARS[content[i]][0].length; x++) {
-        for (var y = 0; y < CHARS[content[i]].length; y++) {
-          if (CHARS[content[i]][y][x] > 0) {
-            ctx.fillStyle = 'orange';
-          }
-          else {
-            ctx.fillStyle = '#262626';
-          }
-          ctx.beginPath();
-          ctx.arc(DOT_RADIUS+(DOT_RADIUS*2*(x+i*6)), DOT_RADIUS+(DOT_RADIUS*2*(y+1)), DOT_RADIUS-1, 0, 2*Math.PI);
-          ctx.closePath();
-          ctx.fill();
+    var matrix = addLineSpacing(kern(content));
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] > 0) {
+          ctx.fillStyle = 'orange';
         }
+        else {
+          ctx.fillStyle = '#262626';
+        }
+        ctx.beginPath();
+        ctx.arc(DOT_RADIUS+(DOT_RADIUS*2*j), DOT_RADIUS+(DOT_RADIUS*2*i), DOT_RADIUS-2, 0, 2*Math.PI);
+        ctx.closePath();
+        ctx.fill();
       }
     }
+
 
   }
   window.requestAnimationFrame(draw);
 }
 window.requestAnimationFrame(draw);
+
+function kern(text) {
+  var matrix = [[],[],[],[],[],[],[]];
+  for (var i = 0; i < text.length; i++) {
+    var gap = false;
+    if (i < 1) {
+      gap = false;
+    }
+    else {
+      for (var j = 0; j < 7; j++) {
+        if (CHARS[text[i-1]][j].last() > 0 && (CHARS[text[i]][j][0] > 0 ||
+                                              (CHARS[text[i]][j-1] && CHARS[text[i]][j-1][0] > 0) ||
+                                              (CHARS[text[i]][j+1] && CHARS[text[i]][j+1][0] > 0))) {
+          gap = true;
+          break;
+        }
+      }
+    }
+    for (var k = 0; k < 7; k++) {
+      matrix[k] = matrix[k].concat(gap ? [0] : []).concat(CHARS[text[i]][k]);
+    }
+  }
+  return matrix;
+}
+
+function addLineSpacing(line) {
+  return ['0'.repeat(line[0].length).split('')].concat(line);
+}
