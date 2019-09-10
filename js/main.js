e353a8134c7a902ca4cd456b500fbcd5ae13f65d
Botond Diviki-Nagy <boti0909@gmail.com>
2019-09-10 13:08:42 +0200

    api functionality and visual fixes

--- js/main.js
+++ js/main.js
@@ -4,24 +4,35 @@ if (!Array.prototype.last){
     };
 };
 
-const DOT_RADIUS = 6;
+const DOT_RADIUS = 4;
 const ROWS = 20;
 const COLUMNS = 50;
 
-var content = "44  Deák   1'";
+var times = [];
+
+var content = [];
+queryApi();
+// window.setInterval(function() {
+//   queryApi();
+// }, 20000);
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+canvas.height = (DOT_RADIUS*2) * 8 * 5;
+canvas.width = Math.max(window.innerWidth - 100, 1000);
+const width = Math.floor(canvas.width / (DOT_RADIUS+2));
 
 var last = performance.now();
 var state = 0;
 
 function draw() {
   if (last < performance.now() - 1000) {
+    state = (state + 1) % 2;
     last = performance.now();
     ctx.fillStyle = '#1a1a1a';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    var matrix = addLineSpacing(kern(content));
+    var matrix = matrixify(content);
     for (var i = 0; i < matrix.length; i++) {
       for (var j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] > 0) {
@@ -31,12 +42,11 @@ function draw() {
           ctx.fillStyle = '#262626';
         }
         ctx.beginPath();
-        ctx.arc(DOT_RADIUS+(DOT_RADIUS*2*j), DOT_RADIUS+(DOT_RADIUS*2*i), DOT_RADIUS-2, 0, 2*Math.PI);
+        ctx.arc(DOT_RADIUS-1+((DOT_RADIUS-1)*2*j), DOT_RADIUS+(DOT_RADIUS*2*i), DOT_RADIUS-2, 0, 2*Math.PI);
         ctx.closePath();
         ctx.fill();
       }
     }
-
 
   }
   window.requestAnimationFrame(draw);
@@ -45,12 +55,34 @@ window.requestAnimationFrame(draw);
 
 function kern(text) {
   var matrix = [[],[],[],[],[],[],[]];
+  if (text.split('').last() == "†" && state == 1) {
+    for (var k = 0; k < 7; k++) {
+      matrix[k] = matrix[k].concat('0'.repeat(width).split(''));
+    }
+    return matrix;
+  }
   for (var i = 0; i < text.length; i++) {
+    if (text[i] == '†') {
+      var rest = kern(text.slice(i+1));
+      var space = width - (rest[0].length + matrix[0].length);
+      for (var k = 0; k < 7; k++) {
+        matrix[k] = matrix[k].concat('0'.repeat(Math.max(0, space)).split('')).concat(rest[k]);
+      }
+      break;
+    }
+    if (text[i] == '¥') {
+      var lengthsofar = matrix[0].length;
+      for (var k = 0; k < 7; k++) {
+        matrix[k] = matrix[k].concat('0'.repeat((5*7) - lengthsofar).split(''));
+      }
+      continue;
+    }
+    if (!Object.keys(CHARS).includes(text[i])) {console.log(text[i]); text = text.replace(text[i], '-')}
     var gap = false;
     if (i < 1) {
       gap = false;
     }
-    else {
+    else if (text[i-1] != '†' && text[i-1] != '¥'){
       for (var j = 0; j < 7; j++) {
         if (CHARS[text[i-1]][j].last() > 0 && (CHARS[text[i]][j][0] > 0 ||
                                               (CHARS[text[i]][j-1] && CHARS[text[i]][j-1][0] > 0) ||
@@ -69,4 +101,17 @@ function kern(text) {
 
 function addLineSpacing(line) {
   return ['0'.repeat(line[0].length).split('')].concat(line);
+}
+
+function matrixify(lines) {
+  var matrix = [];
+  for (var i = 0; i < lines.length; i++) {
+    matrix = matrix.concat(addLineSpacing(kern(lines[i])));
+  }
+  if (lines.length < 5) {
+    for (var i = 0; i < (5 - lines.length) * 8; i++) {
+      matrix = matrix.concat(['0'.repeat(width).split('')]);
+    }
+  }
+  return matrix;
 }
